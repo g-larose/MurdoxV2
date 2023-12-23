bd80bf1c87c0d4feac08510b4ea53b3db65f8bb7
g-larose <larose.g@fromtier.com>
2023-12-23 06:19:32 -0500

    Add project files.

---
+++ MurdoxV2/Bot.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.SlashCommands;
+
+namespace MurdoxV2
+{
+    public class Bot
+    {
+        DiscordClient? client { get; set; }
+        public CommandsNextExtension? Commands { get; set; }
+        public SlashCommandsExtension? SlashCommands { get; set; }
+        public static InteractivityExtension? Interactivity { get; set; }
+        public static Bot Instance { get; } = new Bot();
+
+        public Bot()
+        {
+
+        }
+
+        #region RUN
+
+        public async Task RunAsync()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ MurdoxV2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MurdoxV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
